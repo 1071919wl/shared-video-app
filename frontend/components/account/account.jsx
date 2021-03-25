88bd43d2809fe3462f603c4c5603dc63fd70dad7
William Leung <w.leung.93@gmail.com>
2021-03-25 16:53:23 -0700

    prep for push

--- frontend/components/account/account.jsx
+++ frontend/components/account/account.jsx
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import Clip from '../clip/clip'
+import { fetchClips } from '../../actions/clip_actions';
 import {postClip} from '../../actions/clip_actions';
 import NavBar from '../nav/navbar';
+import { Link } from 'react-router-dom';
 
 
 const Account = () => {
@@ -10,9 +11,13 @@ const Account = () => {
     const [title, setTitle] = useState('');
     const [videoFile, setVideoFile] = useState(null);
 
-    const user_id = useSelector(state => state.session.id)
-    const dispatch = useDispatch()
+    const user_id = useSelector(state => state.session.id);
+    const allClips = useSelector(state => state.entities.clips);
+    const dispatch = useDispatch();
 
+    useEffect(() => {
+        dispatch(fetchClips());
+    }, [])
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -33,19 +38,63 @@ const Account = () => {
         setVideoFile(e.currentTarget.files[0]);
     }
 
+    const uploadedClips = (clip) => {
+
+        if(clip.user_id === user_id){
+            return(
+                <div key={clip.id}>
+                    {clip.title}
+                    <div key={clip.id}>
+                        <Link to={`/clip/${clip.id}`}>
+                            <video type="video/mp4" src={clip.video_clip} width="400" height="auto" />
+                        </Link>
+                    </div>
+                </div>
+            )
+        }
+        
+        // clips.map((clip) => {
+        //     return(
+                // <div key={clip.id}>
+                //     {clip.title}
+                //     <div key={clip.id}>
+                //         <Link to={`/clip/${clip.id}`}>
+                //             <video type="video/mp4" src={clip.video_clip} width="400" height="auto" />
+                //         </Link>
+                //     </div>
+                // </div>
+        //     )
+        // })
+    }
+
     return (
         <div>
             <NavBar />
-            Account page
-            <form onSubmit={handleSubmit}>
-                <label>Video title:
-                    <input type="text" onChange={e => setTitle(e.target.value)}/>
-                </label>
-                <label>Add a video:
-                    <input id="file-upload" type="file" onChange={handleFile}/>
-                </label>
-                <button type='submit'>Upload</button>
-            </form>
+            <div className="account_container">
+                Account page
+                <form onSubmit={handleSubmit} className='form_container'>
+                    <label>Video title:
+                        <input type="text" onChange={e => setTitle(e.target.value)} className='account_input'/>
+                    </label>
+                    <label>Add a video:
+                        <input id="file-upload" type="file" onChange={handleFile} className='account_file'/>
+                    </label>
+                    <button type='submit' className='account_button'>Upload</button>
+                </form>
+            </div>
+            <div>
+                Clips uploaded
+                {Array.isArray(allClips) ?
+                <div>
+                    Clip Index
+                    {allClips.map((clip) => (
+                        uploadedClips(clip)
+                    ))}
+                </div>
+                :
+                    <div></div>                
+                }
+            </div>
         </div>
     )
 
